ba8bc7ea3ce89df5d649044a45c4a04c9b2fb1da
Guilherme Prado <L00188300@atu.ie>
2025-05-02 20:09:25 +0100

    Creating base project to work with the DevOps pipeline workflow implementation

---
+++ eKIBRA.Web.Test/Test1.cs
@@ -0,0 +1,12 @@
+﻿namespace eKIBRA.Web.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
