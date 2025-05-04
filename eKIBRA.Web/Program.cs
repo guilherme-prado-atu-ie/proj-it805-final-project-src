28e40704d5e560d08993f60d4a388f5cccc281e2
Guilherme Prado <L00188300@atu.ie>
2025-05-04 21:39:55 +0100

    Simplify configuration and cleanup workflow triggers
    Removed redundant comments and unused triggers in GitHub workflows for improved clarity and maintainability. Also streamlined the connection string retrieval logic in `Program.cs` to remove unnecessary blank lines.

--- eKIBRA.Web/Program.cs
+++ eKIBRA.Web/Program.cs
@@ -12,8 +12,6 @@ public class Program
 
         // Add services to the container.
         var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
-                               
-                               
                                ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
         
         builder.Services.AddDbContext<ApplicationDbContext>(options =>
