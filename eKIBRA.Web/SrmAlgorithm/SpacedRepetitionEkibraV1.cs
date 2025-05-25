d7c4e48b9f182af9633e29eed4826efac31e88f5
Guilherme Prado <L00188300@atu.ie>
2025-05-25 11:32:09 +0100

    - Added `SpacedRepetitionEKibraV1Input` class in `SpacedRepetitionEkibraV1.cs` to structure input data for spaced repetition logic. - Implemented `CreateListOfFlashcardProgress` method in `SpacedRepetitionEkibraV1.cs` to initialize flashcard progress list. - Replaced "Edit" link with "Study" in `Index.cshtml` for improved navigation clarity. - Updated `README.md` with detailed project overview, setup instructions, technology stack, and contributing guidelines.

--- eKIBRA.Web/SrmAlgorithm/SpacedRepetitionEkibraV1.cs
+++ eKIBRA.Web/SrmAlgorithm/SpacedRepetitionEkibraV1.cs
@@ -2,9 +2,28 @@
 
 namespace eKIBRA.Web.SrmAlgorithm;
 
+
+public class SpacedRepetitionEKibraV1Input
+{
+    public string Id { get; set; }
+    public string UserId { get; set; }
+    public string DeckId { get; set; }
+} 
+
 public class SpacedRepetitionEKibraV1
 {
     public virtual required string Id { get; set; }
+
+    public virtual List<FlashcardProgress> CreateListOfFlashcardProgress(SpacedRepetitionEKibraV1Input input)
+    {
+        /*
+           Id = Guid.NewGuid().           
+           UserId = user.Id,
+           DeckId = Input.DeckId,
+         */
+        
+        return new();
+    } 
 
     public virtual int GetNextInterval(FlashcardProgress flashcardProgress)
     {
